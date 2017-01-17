d1cf92077b93132288992678a6f2714ab8ceba32
Wei Zeng <gzzengwei@gmail.com>
2017-01-17 16:20:17 +1100

    mix new

---
+++ lib/weather_obs.ex
@@ -0,0 +1,18 @@
+defmodule WeatherObs do
+  @moduledoc """
+  Documentation for WeatherObs.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> WeatherObs.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/weather_obs_test.exs
@@ -0,0 +1,8 @@
+defmodule WeatherObsTest do
+  use ExUnit.Case
+  doctest WeatherObs
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
